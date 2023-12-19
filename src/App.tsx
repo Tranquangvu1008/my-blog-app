7f215fef0b9a91af996bea233713d81012cf2ff3
VuMac <tranquangvu1008@gmail.com>
2023-12-19 19:07:29 +0700

    update package

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { useDispatch } from "react-redux";
-import { getAttributeRequest } from "./redux/test/action";
 
 function App() {
-  const dispacth = useDispatch();
-
-  useEffect(() => {
-    dispacth(getAttributeRequest(1, 2));
-  }, [dispacth]);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
