3f9411920c032064d6f6e83fd0ca411ec01c6f6d
VuMac <tranquangvu1008@gmail.com>
2024-01-10 09:26:33 +0700

    update

--- src/page/Product/index.tsx
+++ src/page/Product/index.tsx
@@ -1,14 +1,31 @@
-import { useRef, useState } from "react";
+import { useReducer, useRef, useState } from "react";
 
-type ProductProps = {
+export type ProductProps = {
     name: string;
     code: string;
+}
+
+const countReducer = (state: any, action: any) => {
+    console.log(state);
+    console.log(action);
+
+
+    if (action.type === "inc") {
+        return {
+            product: {
+                data: [...state.product.data, { name: action.state.name, code: action.state.code }]
+            }
+        }
+    }
+    throw Error('Unknown action.');
 }
 
 const Product = () => {
     const [list, setList] = useState<ProductProps[]>([]);
     const nameRef = useRef<HTMLInputElement | null>(null)
     const codeRef = useRef<HTMLInputElement | null>(null)
+
+    const [state, dispatch] = useReducer(countReducer, { product: { data: [] } });
 
     const addNewProduct = () => {
         if (nameRef.current && codeRef.current) {
@@ -31,17 +48,22 @@ const Product = () => {
             <input ref={nameRef} id="name" defaultValue={"hehe"} />
             <label htmlFor="name">Code</label>
             <input ref={codeRef} id="code" defaultValue={"code"} />
-            <button onClick={addNewProduct}>Add</button>
+            <button onClick={() => {
+                dispatch(
+                    { type: "inc", state: { name: nameRef.current?.value, code: codeRef.current?.value } }
+                )
+            }}>Add</button>
             <table>
                 <thead>
                     <tr>
                         <th>No</th>
                         <th>Name</th>
                         <th>Code</th>
+                        <th>Delete</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {list && list.map((item, index) => {
+                    {state.product.data && state.product.data.map((item, index) => {
                         return <tr key={index}>
                             <td>{++index}</td>
                             <td>{item.name}</td>
